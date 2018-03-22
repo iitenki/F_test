cc07fe52faaa6b1116e9534ad06bee1931d9c02f
RYUU <BOKUNOAILR@GMAIL.COM>
2018-03-22 16:15:33 +0800

    Session扩展

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 # -*- coding:utf8 -*-
 
 import redis
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
+from flask_session import Session
 
 
 class Config(object):
@@ -23,6 +24,17 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
+    # Session扩展的配置
+    # 将session保存到redis中
+    # 如果没有设置SESSION_REDIS, 扩展会自动按照redis默认配置进行创建,
+    SESSION_TYPE = "redis"
+    # 手动创建
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 签名
+    SESSION_USE_SIGNER = True
+    # 过期时间: 单位 秒
+    PERMANENT_SESSION_LIFETIME = 86400
+
 
 
 app = Flask(__name__)
@@ -39,11 +51,15 @@ redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 集成CSRF保护: 校验cookie中的CSRF和表单中提交过来的CSRF是否一样
 csrf = CSRFProtect(app)
 
+# 集成session
+Session(app)
+
 
 
 
 @app.route("/index", methods=["GET", "POST"])
 def index():
+    session["usersession"] = "asession"
     redis_store.set("name", "one")
     return "index"
 
